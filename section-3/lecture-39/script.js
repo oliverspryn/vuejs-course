d0c08c9c6ab3cbb6897e17d67114a89966319308
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2018-07-21 18:22:34 -0400

    Add Section 3, Lecture 39

---
+++ section-3/lecture-39/script.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    data: {
+        ingredients: [ 'meat', 'fruit', 'cookies' ],
+        persons: [
+            { name: 'Oliver', age: 25, color: 'blue' },
+            { name: 'Ian', age: 'unknown', color: 'red' }
+        ]
+    }
+});
